e8f4b718d6f26194efceb68984c72091444d6a92
Coomba <coombapace@gmail.com>
2019-07-24 14:40:26 -0400

    Updates

---
+++ precache-manifest.8c3123cf71516d8b1ce0a4952e03883c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0406cf3fc8e19f46a9a3be493517761",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "21dad06fdbd418879db5",
+    "url": "/homepage/static/css/main.b47f55a0.chunk.css"
+  },
+  {
+    "revision": "b612b08ebb042a43de82",
+    "url": "/homepage/static/js/2.7662ae86.chunk.js"
+  },
+  {
+    "revision": "21dad06fdbd418879db5",
+    "url": "/homepage/static/js/main.44183c25.chunk.js"
+  },
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  },
+  {
+    "revision": "de45488e5dcfa4ff434acfc54fea3972",
+    "url": "/homepage/static/media/awnews.de45488e.png"
+  },
+  {
+    "revision": "af77d5f00cd0c859993b6d2b9e5a54c7",
+    "url": "/homepage/static/media/bamazon.af77d5f0.png"
+  },
+  {
+    "revision": "ca9e2397d1fa31d076c18b148fcf899d",
+    "url": "/homepage/static/media/battlearena.ca9e2397.png"
+  },
+  {
+    "revision": "640467939a8ec0f3e15629b5813f380b",
+    "url": "/homepage/static/media/iStock.64046793.png"
+  },
+  {
+    "revision": "01bdf4b7ae0a80b6150f7abced85b20c",
+    "url": "/homepage/static/media/liri.01bdf4b7.png"
+  },
+  {
+    "revision": "1564e8ddfdbabe94ce2fe836d654a4ef",
+    "url": "/homepage/static/media/memory_clicker.1564e8dd.png"
+  },
+  {
+    "revision": "c14e772b4fa2ee98362f017c68a00c03",
+    "url": "/homepage/static/media/voltron_screen.c14e772b.png"
+  }
+]);
