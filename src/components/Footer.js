3a14750575904134181877756e04266070b691f9
Coomba <coombapace@gmail.com>
2019-09-09 12:57:10 -0400

    added links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,11 @@ class Footer extends React.Component {
     return (
       <div className="footer-container">
         <div className='footer-icons'>
-          <i className="fab fa-github" id='github-icon'></i><i className="fab fa-linkedin"></i><i className="fas fa-envelope-square" id='email-icon'></i>
+          <a href={'https://github.com/CoombaPace'}> <i className="fab fa-github" id='github-icon'> </i></a>
+          <a href={'https://www.linkedin.com/in/mattia-pace/'}> <i className="fab fa-linkedin"> </i></a>
+          <a href={'https://medium.com/@coombapace'}> <i class="fab fa-medium"> </i></a>
+          <a href={'https://twitter.com/coombapace'}> <i class="fab fa-twitter-square"> </i></a> 
+          {/* <i className="fas fa-envelope-square" id='email-icon'></i> */}
         </div>
       </div>
     )
